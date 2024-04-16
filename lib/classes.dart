b4974e26d26a997d1b6b1d05c06ba7037c31cd21
azm main <a13176802416@163.com>
2024-04-16 04:52:54 +0000

    update

--- lib/classes.dart
+++ lib/classes.dart
@@ -83,3 +83,495 @@ void main() {
   assert(2.8 < distance && distance < 2.9);
   print(distance);
 }
+
+
+
+
+
+
+
+// 构造方法
+
+class Point {
+  double x = 0;
+  double y = 0;
+
+  // Generative constructor with initializing formal parameters:
+  Point(this.x, this.y);
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  // Sets the x and y instance variables
+  // before the constructor body runs.
+  Point(this.x, this.y);
+}
+
+const double xOrigin = 0;
+const double yOrigin = 0;
+
+class Point {
+  final double x;
+  final double y;
+
+  // Sets the x and y instance variables
+  // before the constructor body runs.
+  Point(this.x, this.y);
+
+  // Named constructor
+  Point.origin()
+      : x = xOrigin,
+        y = yOrigin;
+}
+
+class Vector2d {
+  final double x;
+  final double y;
+
+  Vector2d(this.x, this.y);
+}
+
+class Vector3d extends Vector2d {
+  final double z;
+
+  // Forward the x and y parameters to the default super constructor like:
+  // Vector3d(final double x, final double y, this.z) : super(x, y);
+  Vector3d(super.x, super.y, this.z);
+}
+
+// Initializer list sets instance variables before
+// the constructor body runs.
+Point.fromJson(Map<String, double> json)
+    : x = json['x']!,
+      y = json['y']! {
+  print('In Point.fromJson(): ($x, $y)');
+}
+
+Point.withAssert(this.x, this.y) : assert(x >= 0) {
+  print('In Point.withAssert(): ($x, $y)');
+}
+
+class Point {
+  double x, y;
+
+  // The main constructor for this class.
+  Point(this.x, this.y);
+
+  // Delegates to the main constructor.
+  Point.alongXAxis(double x) : this(x, 0);
+}
+
+class ImmutablePoint {
+  static const ImmutablePoint origin = ImmutablePoint(0, 0);
+
+  final double x, y;
+
+  const ImmutablePoint(this.x, this.y);
+}
+
+class Logger {
+  final String name;
+  bool mute = false;
+
+  // _cache is library-private, thanks to
+  // the _ in front of its name.
+  static final Map<String, Logger> _cache = <String, Logger>{};
+
+  factory Logger(String name) {
+    return _cache.putIfAbsent(name, () => Logger._internal(name));
+  }
+
+  factory Logger.fromJson(Map<String, Object> json) {
+    return Logger(json['name'].toString());
+  }
+
+  Logger._internal(this.name);
+
+  void log(String msg) {
+    if (!mute) print(msg);
+  }
+}
+
+var logger = Logger('UI');
+logger.log('Button clicked');
+
+var logMap = {'name': 'UI'};
+var loggerJson = Logger.fromJson(logMap);
+
+
+
+
+
+// 成员方法
+
+import 'dart:math';
+
+class Point {
+  final double x;
+  final double y;
+
+  // Sets the x and y instance variables
+  // before the constructor body runs.
+  Point(this.x, this.y);
+
+  double distanceTo(Point other) {
+    var dx = x - other.x;
+    var dy = y - other.y;
+    return sqrt(dx * dx + dy * dy);
+  }
+}
+
+class Vector {
+  final int x, y;
+
+  Vector(this.x, this.y);
+
+  Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
+  Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Vector && x == other.x && y == other.y;
+
+  @override
+  int get hashCode => Object.hash(x, y);
+}
+
+void main() {
+  final v = Vector(2, 3);
+  final w = Vector(2, 2);
+
+  assert(v + w == Vector(4, 5));
+  assert(v - w == Vector(0, 1));
+}
+
+class Rectangle {
+  double left, top, width, height;
+
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  // Define two calculated properties: right and bottom.
+  double get right => left + width;
+  set right(double value) => left = value - width;
+  double get bottom => top + height;
+  set bottom(double value) => top = value - height;
+}
+
+void main() {
+  var rect = Rectangle(3, 4, 20, 15);
+  assert(rect.left == 3);
+  rect.right = 12;
+  assert(rect.left == -8);
+}
+
+abstract class Doer {
+  // Define instance variables and methods...
+
+  void doSomething(); // Define an abstract method.
+}
+
+class EffectiveDoer extends Doer {
+  void doSomething() {
+    // Provide an implementation, so the method is not abstract here...
+  }
+}
+
+
+
+
+
+// 继承
+
+class Television {
+  void turnOn() {
+    _illuminateDisplay();
+    _activateIrSensor();
+  }
+  // ···
+}
+
+class SmartTelevision extends Television {
+  void turnOn() {
+    super.turnOn();
+    _bootNetworkInterface();
+    _initializeMemory();
+    _upgradeApps();
+  }
+  // ···
+}
+
+class Television {
+  // ···
+  set contrast(int value) {...}
+}
+
+class SmartTelevision extends Television {
+  @override
+  set contrast(num value) {...}
+  // ···
+}
+
+class A {
+  // Unless you override noSuchMethod, using a
+  // non-existent member results in a NoSuchMethodError.
+  @override
+  void noSuchMethod(Invocation invocation) {
+    print('You tried to use a non-existent member: '
+        '${invocation.memberName}');
+  }
+}
+
+
+
+
+// 混入
+
+class Musician extends Performer with Musical {
+  // ···
+}
+
+class Maestro extends Person with Musical, Aggressive, Demented {
+  Maestro(String maestroName) {
+    name = maestroName;
+    canConduct = true;
+  }
+}
+
+mixin Musical {
+  bool canPlayPiano = false;
+  bool canCompose = false;
+  bool canConduct = false;
+
+  void entertainMe() {
+    if (canPlayPiano) {
+      print('Playing piano');
+    } else if (canConduct) {
+      print('Waving hands');
+    } else {
+      print('Humming to self');
+    }
+  }
+}
+
+class Musician {
+  // ...
+}
+mixin MusicalPerformer on Musician {
+  // ...
+}
+class SingerDancer extends Musician with MusicalPerformer {
+  // ...
+}
+
+abstract mixin class Musician {
+  // No 'on' clause, but an abstract method that other types must define if 
+  // they want to use (mix in or extend) Musician: 
+  void playInstrument(String instrumentName);
+
+  void playPiano() {
+    playInstrument('Piano');
+  }
+  void playFlute() {
+    playInstrument('Flute');
+  }
+}
+
+class Virtuoso with Musician { // Use Musician as a mixin
+  void playInstrument(String instrumentName) {
+    print('Plays the $instrumentName beautifully');
+  }  
+} 
+
+class Novice extends Musician { // Use Musician as a class
+  void playInstrument(String instrumentName) {
+    print('Plays the $instrumentName poorly');
+  }  
+}
+
+
+
+
+// 枚举
+
+enum Color { red, green, blue }
+
+enum Vehicle implements Comparable<Vehicle> {
+  car(tires: 4, passengers: 5, carbonPerKilometer: 400),
+  bus(tires: 6, passengers: 50, carbonPerKilometer: 800),
+  bicycle(tires: 2, passengers: 1, carbonPerKilometer: 0);
+
+  const Vehicle({
+    required this.tires,
+    required this.passengers,
+    required this.carbonPerKilometer,
+  });
+
+  final int tires;
+  final int passengers;
+  final int carbonPerKilometer;
+
+  int get carbonFootprint => (carbonPerKilometer / passengers).round();
+
+  bool get isTwoWheeled => this == Vehicle.bicycle;
+
+  @override
+  int compareTo(Vehicle other) => carbonFootprint - other.carbonFootprint;
+}
+
+final favoriteColor = Color.blue;
+if (favoriteColor == Color.blue) {
+  print('Your favorite color is blue!');
+}
+
+assert(Color.red.index == 0);
+assert(Color.green.index == 1);
+assert(Color.blue.index == 2);
+
+List<Color> colors = Color.values;
+assert(colors[2] == Color.blue);
+
+var aColor = Color.blue;
+
+switch (aColor) {
+  case Color.red:
+    print('Red as roses!');
+  case Color.green:
+    print('Green as grass!');
+  default: // Without this, you see a WARNING.
+    print(aColor); // 'Color.blue'
+}
+
+print(Color.blue.name); // 'blue'
+
+print(Vehicle.car.carbonFootprint);
+
+
+
+// 扩展方法
+
+extension NumberParsing on String {
+  int parseInt() {
+    return int.parse(this);
+  }
+
+  double parseDouble() {
+    return double.parse(this);
+  }
+}
+
+extension on String {
+  bool get isBlank => trim().isEmpty;
+}
+
+extension MyFancyList<T> on List<T> {
+  int get doubleLength => length * 2;
+  List<T> operator -() => reversed.toList();
+  List<List<T>> split(int at) => [sublist(0, at), sublist(at)];
+}
+
+
+
+
+
+// 扩展类型
+
+extension type IdNumber(int id) {
+  // Wraps the 'int' type's '<' operator:
+  operator <(IdNumber other) => id < other.id;
+  // Doesn't declare the '+' operator, for example,
+  // because addition does not make sense for ID numbers.
+}
+
+void main() {
+  // Without the discipline of an extension type,
+  // 'int' exposes ID numbers to unsafe operations:
+  int myUnsafeId = 42424242;
+  myUnsafeId = myUnsafeId + 10; // This works, but shouldn't be allowed for IDs.
+
+  var safeId = IdNumber(42424242);
+  safeId + 10; // Compile-time error: No '+' operator.
+  myUnsafeId = safeId; // Compile-time error: Wrong type.
+  myUnsafeId = safeId as int; // OK: Run-time cast to representation type.
+  safeId < IdNumber(42424241); // OK: Uses wrapped '<' operator.
+}
+
+extension type E(int i) {
+  E.n(this.i);
+  E.m(int j, String foo) : i = j + foo.length;
+}
+
+void main() {
+  E(4); // Implicit unnamed constructor.
+  E.n(3); // Named constructor.
+  E.m(5, "Hello!"); // Named constructor with additional parameters.
+}
+
+extension type const E._(int it) {
+  E(): this._(42);
+  E.otherName(this.it);
+}
+
+void main2() {
+  E();
+  const E._(2);
+  E.otherName(3);
+}
+
+extension type E._(int i) {
+  E.fromString(String foo) : i = int.parse(foo);
+}
+
+extension type NumberE(int value) {
+  // Operator:
+  NumberE operator +(NumberE other) =>
+      NumberE(value + other.value);
+  // Getter:
+  NumberE get myNum => this;
+  // Method:
+  bool isValid() => !value.isNegative;
+}
+
+extension type const Opt<T>._(({T value})? _) { 
+  const factory Opt(T value) = Val<T>;
+  const factory Opt.none() = Non<T>;
+}
+extension type const Val<T>._(({T value}) _) implements Opt<T> { 
+  const Val(T value) : this._((value: value));
+  T get value => _.value;
+}
+extension type const Non<T>._(Null _) implements Opt<Never> {
+  const Non() : this._(null);
+}
+
+extension type NumberT(int value) 
+  implements int {
+  // Doesn't explicitly declare any members of 'int'.
+  NumberT get i => this;
+}
+
+void main () {
+  // All OK: Transparency allows invoking `int` members on the extension type:
+  var v1 = NumberT(1); // v1 type: NumberT
+  int v2 = NumberT(2); // v2 type: int
+  var v3 = v1.i - v1;  // v3 type: int
+  var v4 = v2 + v1; // v4 type: int
+  var v5 = 2 + v1; // v5 type: int
+  // Error: Extension type interface is not available to representation type
+  v2.i;
+}
+
+void main() {
+  var n = NumberE(1);
+
+  // Run-time type of 'n' is representation type 'int'.
+  if (n is int) print(n.value); // Prints 1.
+
+  // Can use 'int' methods on 'n' at run time.
+  if (n case int x) print(x.toRadixString(10)); // Prints 1.
+  switch (n) {
+    case int(:var isEven): print("$n (${isEven ? "even" : "odd"})"); // Prints 1 (odd).
+  }
+}
+
